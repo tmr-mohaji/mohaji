8da7fa7895f67bafe86948a4aca7ba33dd50adc2
taecongs <bks0927w@gmail.com>
2022-10-18 16:10:29 +0900

    feat: 헤더 UI 임시 작업  및 공통 commons.scss 생성

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Header.scss';
+
+
+const Header = () => {
+    const [isMenu, setMenu] = useState(false);
+
+    const hambugerBtn = () => {
+        setMenu(isMenu => !isMenu);
+    }
+
+    return (
+        <header>
+            <div className='headerArea'>
+                <div className='head_wrap'>
+                    <h1 className='logo'>
+                        <Link to='/'>로고</Link>
+                    </h1>
+
+                    <ul class="navbar_1 navbar_pc">
+                        <li>
+                            <Link to='/'> 서브 메뉴1 </Link>
+                        </li>
+
+                        <li>
+                            <Link to='/'> 서브 메뉴2 </Link>
+                        </li>
+
+                        <li>
+                            <Link to='/'> 서브 메뉴3 </Link>
+                        </li>
+                    </ul>
+
+                    <div className='nav'>
+                        <ul class="navbar_2 navbar_pc">
+                            <li>
+                                <Link to='/'> 로그인 </Link>
+                            </li>
+
+                            <li>
+                                <Link to='/'> 회원가입 </Link>
+                            </li>
+                        </ul>
+
+                        <div className='nav_icon' onClick={hambugerBtn}>
+                            <div className='nav_hambuger'></div>
+                        </div>
+
+                        <div className={isMenu ? 'mob_box' : 'hide_box'}>
+                            <ul className="navbar_3 navbar_mobile">
+                                <li className='mob_li'>
+                                    <Link className='arrow' to='/'> 서브 메뉴1 </Link>
+                                </li>
+
+                                <li className='mob_li'>
+                                    <Link className='arrow' to='/'> 서브 메뉴2 </Link>
+                                </li>
+
+                                <li className='mob_li'>
+                                    <Link className='arrow' to='/'> 서브 메뉴3 </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
