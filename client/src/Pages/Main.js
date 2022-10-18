fc06589fdedc5a99d3cf0b485c120fcec7cf9193
ryeongse25 <seryeong971120@gmail.com>
2022-10-18 09:37:09 +0900

    feat : search

--- client/src/Pages/Main.js
+++ client/src/Pages/Main.js
@@ -1,6 +1,7 @@
 import Component from '../Components/Component';
 import Map from '../Components/Map';
 import ImageMap from '../Components/ImageMap';
+import Button from '../Components/Button';
 
 const Main = () => {
 
@@ -9,6 +10,7 @@ const Main = () => {
             <Component />
             <Map />
             <ImageMap />
+            <Button />
         </div>
     )
 }
