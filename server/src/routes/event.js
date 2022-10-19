d72dca00eab22dc642348728492db24a5daf4ab9
ryeongse25 <seryeong971120@gmail.com>
2022-10-20 02:52:32 +0900

    fix : map

--- server/src/routes/event.js
+++ server/src/routes/event.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const event = require("../controller/EventController");
 
 router.get("/", event.getEvent);
+router.get("/address", event.getAddress);
 
 module.exports = router;
