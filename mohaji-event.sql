e9e5913485a083cb40fa5443cf0684236537494b
ryeongse25 <seryeong971120@gmail.com>
2022-10-19 17:26:11 +0900

    docs : mysql

--- mohaji-event.sql
+++ mohaji-event.sql
@@ -2,6 +2,7 @@ CREATE DATABASE mohaji;
 use mohaji;
 
 drop table event;
+drop table user;
 
 CREATE TABLE event (
 	id int not null primary key auto_increment,
@@ -19,6 +20,11 @@ CREATE TABLE event (
     price varchar(255) not null default "무료"
 );
 
-delete from event where id < 11;
+CREATE TABLE user (
+	id varchar(255) not null primary key,
+    password varchar(255),
+    nickname varchar(255),
+    email varchar(255)
+);
 
 select * from event;
