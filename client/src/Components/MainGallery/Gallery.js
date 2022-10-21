0368990476768f9ef872075e061821f42cf1a759
taecongs <bks0927w@gmail.com>
2022-10-21 15:21:18 +0900

    feat:메인 갤러리 레이아웃 작업 진행중

--- client/src/Components/MainGallery/Gallery.js
+++ client/src/Components/MainGallery/Gallery.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Button from '../Button';
 import axios from 'axios';
+import './Gallery.scss';
 
 const MAIN_PAGE = "http://localhost:8000/";
 
@@ -11,11 +12,11 @@ const Gallery = () => {
 
     const setCategory = (name) => {
         setType(name);
-    } 
+    }
 
     const getData = async () => {
         const response = await axios.get(MAIN_PAGE, {
-            params: {type: type}
+            params: { type: type }
         })
         setData(response.data);
 
@@ -25,23 +26,29 @@ const Gallery = () => {
         getData();
     }, [type])
 
-    return(
+    return (
         <section>
-            <Button name="전체" onClick={setCategory}/>
-            <Button name="축제" onClick={setCategory}/>
-            <Button name="공연" onClick={setCategory}/>
-            <Button name="콘서트" onClick={setCategory}/>
-            <Button name="박람회" onClick={setCategory}/>
-            <Button name="전시" onClick={setCategory}/>
+            <div className='galleryBox'>
+                <div className='buttonBox'>
+                    <Button name="전체" onClick={setCategory} />
+                    <Button name="축제" onClick={setCategory} />
+                    <Button name="공연" onClick={setCategory} />
+                    <Button name="콘서트" onClick={setCategory} />
+                    <Button name="박람회" onClick={setCategory} />
+                    <Button name="전시" onClick={setCategory} />
+                </div>
 
-            {data.map((data) => {
-                return (
-                    <div key={data.id}>
-                        <p>{data.title}</p>
-                        <img src={"./img/" + data.filename} style={{width: "200px"}}/>
-                    </div>
-                )
-            })}
+                <div className='listBox'>
+                    {data.map((data) => {
+                        return (
+                            <div className='listGallery' key={data.id}>
+                                <p>{data.title}</p>
+                                <img src={"./img/" + data.filename} style={{ width: "200px" }} />
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </section>
     )
 }
