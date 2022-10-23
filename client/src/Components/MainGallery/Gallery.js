5a4bb114047c6083d67948cc0eeebb5ff29e1134
taecongs <bks0927w@gmail.com>
2022-10-23 15:05:18 +0900

    feat:메인 갤러리 카테고리 수정

--- client/src/Components/MainGallery/Gallery.js
+++ client/src/Components/MainGallery/Gallery.js
@@ -4,6 +4,8 @@ import Button from '../Button';
 import axios from 'axios';
 import './Gallery.scss';
 
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+
 import h_icon1 from './img/g_tit_01.png';
 import h_icon2 from './img/g_tit_02.png';
 import h_icon3 from './img/g_tit_03.png';
@@ -15,17 +17,45 @@ const Gallery = () => {
     const [type, setType] = useState("전체");
     const [data, setData] = useState([]);
 
+    // 전체 카테고리에 관련된 내용이 활성화 되었을 시 전체 카테고리 active 주기 위해 정의
+    const links = [...document.querySelectorAll('.buttonBox span')];
+    let activeIndex = 0;
+    let currentIndex = 0;
+    let increment = 1;
+
+    links.forEach((link, index) => {
+        link.addEventListener("click", e => {
+            activeIndex = index;
+            let t = setInterval(() => {
+                if (activeIndex > currentIndex) increment = 1;
+                else if (activeIndex < currentIndex) increment = -1;
+                currentIndex += increment;
+
+                links[currentIndex].classList.add("active");
+                if (currentIndex != -1) links[currentIndex - increment].classList.remove("active");
+
+                if (currentIndex == activeIndex) {
+                    e.target.classList.add("active");
+                    increment = 0;
+                    clearInterval(t);
+                }
+            }, 50);
+        });
+    });
+
+
     const setCategory = (name) => {
         setType(name);
     }
+
 
     const getData = async () => {
         const response = await axios.get(MAIN_PAGE, {
             params: { type: type }
         })
         setData(response.data);
-
     }
+
 
     useEffect(() => {
         getData();
@@ -35,27 +65,42 @@ const Gallery = () => {
         <section>
             <div className='galleryBox'>
                 <div className='buttonBox'>
-                    <Button name="전체" onClick={setCategory} />
-                    <Button name="축제" onClick={setCategory} />
-                    <Button name="공연" onClick={setCategory} />
-                    <Button name="콘서트" onClick={setCategory} />
-                    <Button name="박람회" onClick={setCategory} />
-                    <Button name="전시" onClick={setCategory} />
+                    <span className='active'>
+                        <Button name="전체" onClick={setCategory} />
+                    </span>
+
+                    <span className=''>
+                        <Button name="축제" onClick={setCategory} />
+                    </span>
+
+                    <span className=''>
+                        <Button name="공연" onClick={setCategory} />
+                    </span>
+
+                    <span className=''>
+                        <Button name="콘서트" onClick={setCategory} />
+                    </span>
+
+                    <span className=''>
+                        <Button name="박람회" onClick={setCategory} />
+                    </span>
+
+                    <span className=''>
+                        <Button name="전시" onClick={setCategory} />
+                    </span>
                 </div>
-
-
 
                 <div className='listBox'>
                     {data.map((data) => {
                         return (
                             <div className='listGallery' key={data.id}>
                                 <Link to={"/event/" + data.id}>
-                                    <img className='g_img' src={"./img/" + data.filename} />
+                                    <img className='g_img' src={"./img/" + data.filename} alt='' />
                                     <div className='g_hover'>
                                         <p className='g_tit_h1'>{data.title}</p>
-                                        <p className='g_tit_h2'><img className='g_tit_img' src={h_icon1} alit='' /> {data.start_date} - {data.end_date}</p>
-                                        <p className='g_tit_h3'><img className='g_tit_img' src={h_icon2} alit='' /> {data.type}</p>
-                                        <p className='g_tit_h3'><img className='g_tit_img' src={h_icon3} alit='' /> {data.place}</p>
+                                        <p className='g_tit_h2'><img className='g_tit_img' src={h_icon1} alt='' /> {data.start_date} - {data.end_date}</p>
+                                        <p className='g_tit_h3'><img className='g_tit_img' src={h_icon2} alt='' /> {data.type}</p>
+                                        <p className='g_tit_h3'><img className='g_tit_img' src={h_icon3} alt='' /> {data.place}</p>
                                         <p className='g_tit_h4'>{data.detail}</p>
                                     </div>
                                 </Link>
