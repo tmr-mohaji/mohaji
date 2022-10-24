f85d7338cc6b168e324715e7dbf1c38c4e8695f8
ryeongse25 <seryeong971120@gmail.com>
2022-10-24 15:37:58 +0900

    feat: 마이 페이지

--- client/src/Pages/MyPage.js
+++ client/src/Pages/MyPage.js
@@ -1,30 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios';
+
+const USER_URL = "http://localhost:8000/user/";
 
 const MyPage = () => {
 
-    const checkToken = () => {
-        axios({
-            url: 'http://localhost:8000/user/auth',
+    const [id, setId] = useState("");
+    const [data, setData] = useState([]);
+
+    const checkToken = async () => {
+        await axios({
+            url: USER_URL + 'auth',
             headers: {
                 'Authorization': localStorage.getItem("access_token")
             }
         }).then((result) => {
-            console.log( result.data );
+            setId(result.data.id);
+            getData();
         });
     }
 
-    const getData = () => {
-
+    const getData = async () => {
+        let result = await axios.get(USER_URL + "info", {
+            params: {id : id}
+        })
+        setData(result.data);
     }
 
     useEffect(() => {
         checkToken();
-    }, [])
+    })
 
     return (
-        <div>
-
+        <div style={{paddingTop: "100px"}}>
+            {data.id} <br />
+            {data.nickname} <br />
+            {data.email}
         </div>
     )
 }
