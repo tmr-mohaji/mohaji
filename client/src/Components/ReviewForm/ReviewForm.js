18faffe0e8c43a0a8db2e03682a07ebf3f09b1f7
taecongs <bks0927w@gmail.com>
2022-10-27 16:01:51 +0900

    feat:디테일 페이지 평점 레이아웃 수정

--- client/src/Components/ReviewForm/ReviewForm.js
+++ client/src/Components/ReviewForm/ReviewForm.js
@@ -1,5 +1,8 @@
 import { useRef, useState } from 'react';
+import Dropdown from '../Dropdown/Dropdown';
 import './ReviewForm.scss';
+
+import down_arrow from './img/down_ar.png';
 
 const ReviewForm = (props) => {
 
@@ -7,9 +10,11 @@ const ReviewForm = (props) => {
     const file = useRef();
     const img = useRef();
 
-    const drawStar = () => {
-        document.querySelector('.star span').style.width = `${score.current.value * 20}%`;
-    }
+    const [dropdownVisibility, setDropdownVisibility] = useState(false);
+
+    // const drawStar = () => {
+    //     document.querySelector('.star span').style.width = `${score.current.value * 20}%`;
+    // }
 
 
     const imgPreview = () => {
@@ -29,7 +34,7 @@ const ReviewForm = (props) => {
     }
 
     return (
-        <div>
+        <div className='review_Container'>
             <div className='reviewBox1'>
                 {/* <textarea name="comment" onChange={props.onChange}></textarea> */}
                 <p className='review_tit'>리뷰 <span className='review_line'>|</span> <span className='review_tit2'>공연 후기를 작성하실 수 있습니다.</span></p>
@@ -45,22 +50,62 @@ const ReviewForm = (props) => {
             </div>
 
             <div className='reviewBox2'>
-                <span className="star">
+                {/* <span className="star">
                     ★★★★★ <span>★★★★★</span>
                     <input type="range" name="score" value="3" min="0" max="5" step="0.5" onInput={drawStar} ref={score} onChange={props.onChange} />
-                </span>
+                </span> */}
 
-                <div>
-                    <ul>
-                        <li>item 1</li>
-                        <li>item 2</li>
-                        <li>item 3</li>
-                        <li>item 4</li>
-                    </ul>
+                <div className='drop_box'>
+                    <button className='review_star' onClick={e => setDropdownVisibility(!dropdownVisibility)}>
+                        {
+                            dropdownVisibility ? '리뷰' : '리뷰'
+                        }
+
+                        <img src={down_arrow} alt='' />
+                    </button>
+                    <Dropdown visibility={dropdownVisibility}>
+                        <ul className='drop_ul'>
+                            <li>
+                                <span className="star">
+                                    ★★★★★ <span className='one'>★★★★★</span>
+                                </span>
+                                <p className='start_txt'>아주 좋아요</p>
+                            </li>
+
+                            <li>
+                                <span className="star">
+                                    ★★★★★ <span className='two'>★★★★★</span>
+                                </span>
+                                <p className='start_txt'>맘에 들어요</p>
+                            </li>
+
+                            <li>
+                                <span className="star">
+                                    ★★★★★ <span className='three'>★★★★★</span>
+                                </span>
+                                <p className='start_txt'>보통 이에요</p>
+                            </li>
+
+                            <li>
+                                <span className="star">
+                                    ★★★★★ <span className='four'>★★★★★</span>
+                                </span>
+                                <p className='start_txt'>그냥 그래요</p>
+                            </li>
+
+                            <li>
+                                <span className="star">
+                                    ★★★★★ <span className='five'>★★★★★</span>
+                                </span>
+                                <p className='start_txt'>별로에요</p>
+                            </li>
+
+                        </ul>
+                    </Dropdown>
                 </div>
 
 
-                <button onClick={props.onClick}>등록</button>
+                <button className='review_submit' onClick={props.onClick}>등록</button>
             </div>
         </div>
     )
