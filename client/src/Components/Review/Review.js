c5563a9040cd0a41edcaa69749a83ab8fa7c6cb5
ryeongse25 <seryeong971120@gmail.com>
2022-10-28 13:14:01 +0900

    feat: 리뷰 삭제

--- client/src/Components/Review/Review.js
+++ client/src/Components/Review/Review.js
@@ -6,6 +6,7 @@ const Review = (props) => {
             <p>{props.review == null ? "" : props.review}</p>
             <p>{props.date.slice(0, 10)}</p>
             {props.file != false && <img src={"/img/review_img/" + props.file} style={{width: "200px"}}/>}
+            {props.id == props.login_id && <div><button>수정</button><button onClick={props.deleteReview}>삭제</button></div>}
             <hr style={{border: "1px solid black", width: "100%"}}/>
         </div>
     )
