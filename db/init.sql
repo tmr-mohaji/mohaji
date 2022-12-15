be41d622229b70d145f0c5b7bc5a039aff2cfdb6
ryeongse25 <seryeong971120@gmail.com>
2022-12-15 15:59:23 +0900

    study: docker

---
+++ db/init.sql
@@ -0,0 +1,71 @@
+CREATE DATABASE mohaji;
+use mohaji;
+
+CREATE TABLE event (
+	id int not null primary key auto_increment,
+    title varchar(255) not null,
+    url mediumtext,
+    detail mediumtext,
+    type varchar(255) not null,
+    place varchar(255) not null,
+    address mediumtext not null,
+    start_date date not null,
+    end_date date not null,
+    time varchar(255) not null default "홈페이지 참고",
+    people varchar(255) not null default "누구나",
+    filename varchar(255) not null,
+    price varchar(255) not null default "무료"
+);
+
+CREATE TABLE user (
+	id varchar(255) not null primary key,
+    password varchar(255),
+    nickname varchar(255),
+    email varchar(255)
+);
+
+CREATE TABLE event_like (
+    id int not null primary key auto_increment,
+	user_id varchar(255) not null,
+    FOREIGN KEY ( user_id ) REFERENCES user(id) ON DELETE CASCADE,
+	event_id int not null,
+    FOREIGN KEY ( event_id ) REFERENCES event(id) ON DELETE CASCADE
+);
+
+CREATE TABLE review (
+	id int not null primary key auto_increment,
+    user_id varchar(255) not null,
+    FOREIGN KEY ( user_id ) REFERENCES user(id) ON DELETE CASCADE,
+    event_id int not null,
+    FOREIGN KEY ( event_id ) REFERENCES event(id) ON DELETE CASCADE,
+    score decimal(3,2) not null,
+    content mediumtext,
+    createdAt TIMESTAMP,
+    updatedAt TIMESTAMP
+);
+
+CREATE TABLE review_img (
+	id int not null primary key auto_increment,
+    review_id int not null,
+    FOREIGN KEY (review_id) REFERENCES review(id) on DELETE CASCADE,
+    filename varchar(255) not null
+);
+
+CREATE TABLE schedule (
+	id int not null primary key auto_increment,
+    user_id varchar(255) not null,
+    FOREIGN KEY ( user_id ) REFERENCES user(id) ON DELETE CASCADE,
+    event_id int not null,
+    FOREIGN KEY ( event_id ) REFERENCES event(id) ON DELETE CASCADE,
+    date date not null
+);
+
+CREATE TABLE socket (
+	id int not null primary key auto_increment,
+    nickname varchar(255) not null,
+    user_id varchar(255) not null,
+    FOREIGN KEY ( user_id ) REFERENCES user(id) ON DELETE CASCADE,
+	message mediumtext not null,
+    createdAt TIMESTAMP,
+    updatedAt TIMESTAMP
+);
